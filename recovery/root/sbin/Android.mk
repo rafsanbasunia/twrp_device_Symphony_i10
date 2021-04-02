65e9a3a27396e8f6d26a062dc3bfac86e6dd5fb4
rafsanbasunia <rafsanhossainrafsan@gmail.com>
2021-04-02 22:56:19 +0600

    Initial Build

---
+++ recovery/root/sbin/Android.mk
@@ -0,0 +1,7 @@
+# Replace ${_CODENAME_} with your Device CodeName's Value. Mine is Primo_RX5.
+# Replace ${_VENDORNAME_} with your Brand/Vendor/Manufacturer's Value, Mine is WALTON 
+
+ifneq ($(filter i10,$(TARGET_DEVICE)),)
+LOCAL_PATH := device/SYMPHONY/i10
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
---
+++ recovery/root/sbin/omni_i10.mk
@@ -0,0 +1,33 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/${_VENDORNAME_}/${_CODENAME_}/prebuilt/kernel:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := i10
+PRODUCT_NAME := omni_i10
+PRODUCT_BRAND := SYMPHONY
+PRODUCT_MODEL := i10
+PRODUCT_MANUFACTURER := SYMPHONY
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="CUBOT/RAINBOW/RAINBOW:6.0/MRA58K/20200420:user/release-keys" \
+    PRIVATE_BUILD_DESC="full_w363-user 6.0 MRA58K 1590131088 release-keys"
